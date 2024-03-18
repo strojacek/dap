55bdc444a9d5d97c76a087c80d3b6fab1e9734cf
strojacek <s1trojacek@yahoo.com>
2024-03-18 10:24:16 -0500

    Update

--- ErrorListener.hpp
+++ ErrorListener.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "antlr4-runtime.h"
 
 using namespace antlr4;
@@ -6,9 +7,21 @@ using namespace antlrcpp;
 using namespace std;
 
 
-class ErrorListener {
+class ErrorListener : public BaseErrorListener {
 
 		private:
+				ostream& stderr;
 
+		public:
+				ErrorListener(ostream& stderr) : stderr(stderr) {}
 
-}
+				virtual void syntaxError(Recognizer* recognizer,
+										 Token* offendingSymbol,
+										 size_t line,
+										 size_t charPositionInLine,
+										 const string& msg,
+										 exception_ptr e) override {
+		this->stderr << msg << endl;	
+				}
+
+};
